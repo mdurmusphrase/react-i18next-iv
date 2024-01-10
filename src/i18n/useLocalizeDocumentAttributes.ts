adcf7dc42eb946621e4b06fc5bb21bbabbe60af6
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-01-10 18:35:17 +0100

    feat: localize document title

--- src/i18n/useLocalizeDocumentAttributes.ts
+++ src/i18n/useLocalizeDocumentAttributes.ts
@@ -2,12 +2,13 @@ import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 export default function useLocalizeDocumentAttributes() {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   useEffect(() => {
     if (i18n.resolvedLanguage) {
       document.documentElement.lang = i18n.resolvedLanguage;
       document.documentElement.dir = i18n.dir(i18n.resolvedLanguage);
+      document.title = t("app_title");
     }
-  }, [i18n, i18n.resolvedLanguage]);
+  }, [i18n, i18n.resolvedLanguage, t]);
 }
