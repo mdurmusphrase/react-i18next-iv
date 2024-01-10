e41bcc542a7138d1ca9f42eb99bb5f9239231153
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-01-10 15:23:58 +0100

    build: install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import i18nextLogo from "./assets/i18next-logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="w-8 h-8" alt="React logo" />
+        </a>
+        <a href="https://i18next.com" target="_blank">
+          <img src={i18nextLogo} className="w-8 h-8" alt="i18next logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +28,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
