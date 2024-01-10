4bedd07fddc002183a3fbc64cf87ffe99af36ee7
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-01-10 18:05:46 +0100

    feat: add lang icon

--- src/i18n/LocaleSwitcher.tsx
+++ src/i18n/LocaleSwitcher.tsx
@@ -1,21 +1,26 @@
 import { useTranslation } from "react-i18next";
+import LangIcon from "./LangIcon";
 import { supportedLngs } from "./config";
 
 export default function LocaleSwitcher() {
   const { i18n } = useTranslation();
 
   return (
-    <div className="locale-switcher">
-      <select
-        value={i18n.resolvedLanguage}
-        onChange={(e) => i18n.changeLanguage(e.target.value)}
-      >
-        {Object.entries(supportedLngs).map(([code, name]) => (
-          <option value={code} key={code}>
-            {name}
-          </option>
-        ))}
-      </select>
+    <div className="flex items-center">
+      <div className="locale-switcher">
+        <LangIcon />
+
+        <select
+          value={i18n.resolvedLanguage}
+          onChange={(e) => i18n.changeLanguage(e.target.value)}
+        >
+          {Object.entries(supportedLngs).map(([code, name]) => (
+            <option value={code} key={code}>
+              {name}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
