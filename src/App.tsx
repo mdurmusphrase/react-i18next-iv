dcfce3fb911a4ad6ef6189a21dbff6804fa3aeac
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-01-10 15:59:15 +0100

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import { useState } from "react";
-import i18nextLogo from "./assets/i18next-logo.png";
-import reactLogo from "./assets/react.svg";
+import Header from "./layout/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="h-8 w-8" alt="React logo" />
-        </a>
-        <a href="https://i18next.com" target="_blank">
-          <img src={i18nextLogo} className="h-8 w-8" alt="i18next logo" />
-        </a>
-      </div>
-      <h1 className="text-2xl font-bold">React + i18next</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container mx-auto mt-8">
+      <Header />
+    </div>
   );
 }
 
