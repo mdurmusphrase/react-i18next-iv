05fb9a6d51639988de188f95322ddf8bbbf70fe5
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-01-10 18:36:46 +0100

    feat: localize document title

--- src/i18n/useLocalizeDocumentAttributes.ts
+++ src/i18n/useLocalizeDocumentAttributes.ts
@@ -8,7 +8,7 @@ export default function useLocalizeDocumentAttributes() {
     if (i18n.resolvedLanguage) {
       document.documentElement.lang = i18n.resolvedLanguage;
       document.documentElement.dir = i18n.dir(i18n.resolvedLanguage);
-      document.title = t("app_title");
     }
+    document.title = t("app_title");
   }, [i18n, i18n.resolvedLanguage, t]);
 }
