d0a196b51911731a445abacab2b11588f25a7b51
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-01-25 12:58:15 +0100

    docs: add docblock for date formatter

--- src/i18n/formatters.tsx
+++ src/i18n/formatters.tsx
@@ -32,6 +32,14 @@ function qualifiedLngFor(lng: string): string {
   }
 }
 
+/**
+ * Formats a datetime.
+ *
+ * @param value - The datetime to format.
+ * @param lng - The language to format the number in.
+ * @param options - passed to Intl.DateTimeFormat.
+ * @returns The formatted datetime.
+ */
 export function datetime(
   value: Date | number,
   lng: string | undefined,
